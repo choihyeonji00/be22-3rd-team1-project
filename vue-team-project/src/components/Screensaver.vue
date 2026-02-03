8cefdb1bcec6c40799b784bd6cf20b9bd9ea5b98
LimRaynor <morden9137@gmail.com>
2026-02-03 13:29:31 +0900

    tossMedia,기존의 화면보호기 대신 비디오로 변경 + 테스트용 메인화면  대기시간 10초로 변경

--- vue-team-project/src/components/Screensaver.vue
+++ vue-team-project/src/components/Screensaver.vue
@@ -10,7 +10,7 @@ const reset = () => {
   clearTimeout(timer)
   timer = setTimeout(() => {
     show.value = true         // 타이머 시간설정
-  }, 3000)
+  }, 10000)
 }
 
 // 화면 켜짐 조선
@@ -22,15 +22,32 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="show" class="black-box"></div>
+<!--  <div v-if="show" class="black-box"></div> 기존 검정화면 보혿기-->
+
+  <div v-if="show" class="screensaver-overlay" @click="reset">
+    <video
+        src="/media/toss.mp4"
+        autoplay
+        muted
+        loop
+        playsinline
+    ></video>
+  </div>
 </template>
 
 <style scoped>
-.black-box {
+/*.black-box 기존 검정화면 보호기*/
+.screensaver-overlay{
   position: fixed;
   width: 100%;
   height: 100%;
   background: black;
   z-index: 9999; /* 가장 맨앞으로 */
+}
+
+video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* 영상을 화면 비율에 맞춰 꽉 채우기 (빈틈 없이) */
 }
 </style>
